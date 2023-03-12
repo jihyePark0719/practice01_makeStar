bd9b85b6d6bad4ae7b6cbcec31d10bacb9daea92
Jihye Park <parkjihye@bagjihyeui-MacBookPro.local>
2023-03-12 15:53:30 +0900

    피라미드 커밋

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
+
 contract Pyramid {
     function run(uint size) pure public returns (string memory _out) {
         _out = ""; // TODO
@@ -13,5 +15,12 @@ contract Pyramid {
          *     "*****\n"
          *
          */
+         for (uint i=0; i<5; i++){
+            for (uint j=0; j<=i; j++){
+                _out = string.concat(_out, "*");
+            } 
+            _out = string.concat(_out, "\n");      
+        }
+        console.log("Result:\n%s", _out);         
     }
 }
